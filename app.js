5f2b6870bb92d3167bd48aa20eab1c0d2bb0d0b1
maxwu1104 <maxwu1104@gmail.com>
2023-04-17 13:22:46 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //require package in this project
 const express = require('express')
 const app = express()
+const { engine } = require('express-handlebars')
+
+//express templete engine setting
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', './views')
 
 //port setting
 const port = 3000
 
 //router setting
 app.get('/', (req, res) => {
-  res.send('Initial project start.')
+  res.render('index')
 })
 
 //server start and listen
