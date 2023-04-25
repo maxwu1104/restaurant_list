1b88d64945659a33834a2cb0853e809c064583fd
maxwu1104 <maxwu1104@gmail.com>
2023-04-25 14:15:51 +0800

    feat: add connect-flash & partials message

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 // require internal files
 const routes = require('./routes')
@@ -33,9 +34,13 @@ app.use(methodOverride('_method'))
 // authenticated
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.authenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
